0dbe7bf2dcc2541d3ed9b4a1ac8f6fb30c6986ab
TheDriftingWind <CharlesYZhu@yahoo.com>
2017-11-12 12:12:52 -0500

    MainActivity push

--- app/src/main/java/charleszhu/qunnipiac/edu/a2017hackathon/MainActivity.java
+++ app/src/main/java/charleszhu/qunnipiac/edu/a2017hackathon/MainActivity.java
@@ -1,5 +1,6 @@
 package charleszhu.qunnipiac.edu.a2017hackathon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,10 +16,16 @@ public class MainActivity extends AppCompatActivity {
     public void onClick(View v){
         switch(v.getId()){
             case R.id.PoliceButton:
+                Intent i = new Intent(this, PoliceActivity.class);
+                startActivity(i);
                 break;
             case R.id.FireButton:
+                Intent j = new Intent(this, FireActivity.class);
+                startActivity(j);
                 break;
             case R.id.MedicalButton:
+                Intent k = new Intent(this, MedicalActivity.class);
+                startActivity(k);
                 break;
         }
     }
---
+++ app/src/main/java/charleszhu/qunnipiac/edu/a2017hackathon/PoliceActivity.java
@@ -0,0 +1,15 @@
+package charleszhu.qunnipiac.edu.a2017hackathon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PoliceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_police);
+    }
+
+
+}
