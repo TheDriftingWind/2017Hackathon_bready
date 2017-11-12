18715382ea21410c608ceb3064619394d66b825a
TheDriftingWind <CharlesYZhu@yahoo.com>
2017-11-12 13:11:26 -0500

    MainActivity push

--- app/src/main/java/charleszhu/qunnipiac/edu/a2017hackathon/MedicalActivity.java
+++ app/src/main/java/charleszhu/qunnipiac/edu/a2017hackathon/MedicalActivity.java
@@ -1,7 +1,11 @@
 package charleszhu.qunnipiac.edu.a2017hackathon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Switch;
 
 public class MedicalActivity extends AppCompatActivity {
 
@@ -9,5 +13,34 @@ public class MedicalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medical);
+    }
+
+    public void ToggleViews(View v){
+        LinearLayout Sting = (LinearLayout) findViewById(R.id.Stings);
+        LinearLayout Burn = (LinearLayout) findViewById(R.id.Burn);
+        LinearLayout OpenWound = (LinearLayout) findViewById(R.id.OpenWound);
+
+        switch(v.getId()){
+            case R.id.ToggleBurn:
+                if(Burn.getVisibility() == View.VISIBLE){
+                    Burn.setVisibility(View.GONE);
+                } else {
+                    Burn.setVisibility(View.VISIBLE);
+                }
+                break;
+            case R.id.ToggleOpenWound:
+                if(Burn.getVisibility() == View.VISIBLE){
+                    OpenWound.setVisibility(View.GONE);
+                } else {
+                    OpenWound.setVisibility(View.VISIBLE);
+                }
+                break;
+            case R.id.ToggleSting:
+                if(Burn.getVisibility() == View.VISIBLE){
+                    Sting.setVisibility(View.GONE);
+                } else {
+                    Sting.setVisibility(View.VISIBLE);
+                }
+        }
     }
 }
