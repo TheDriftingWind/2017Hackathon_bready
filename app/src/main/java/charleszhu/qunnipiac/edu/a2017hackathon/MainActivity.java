052f11cf00077528e9e8424c580d32dd3dc69e68
TheDriftingWind <CharlesYZhu@yahoo.com>
2017-11-12 11:36:22 -0500

    MainActivity push

--- app/src/main/java/charleszhu/qunnipiac/edu/a2017hackathon/MainActivity.java
+++ app/src/main/java/charleszhu/qunnipiac/edu/a2017hackathon/MainActivity.java
@@ -2,6 +2,7 @@ package charleszhu.qunnipiac.edu.a2017hackathon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.PoliceButton:
+                break;
+            case R.id.FireButton:
+                break;
+            case R.id.MedicalButton:
+                break;
+        }
     }
 }
